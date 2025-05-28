f42c5cfd81d6407e2eac6df80c64d130fe803cf9
Danicalde02 <danicalderon912@gmail.com>
2025-05-28 15:07:35 -0500

    Add files via upload

---
+++ sistemaContratos/src/test/java/proyecto/sistemaContratos/SistemaContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.sistemaContratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
